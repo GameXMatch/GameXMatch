4cb1cbfb892d1793f8369a5f2603ea9916a96a72
Mario Tomic <mario-tomic@hotmail.com>
2022-08-30 11:44:25 +0200

    Messages display on sides

--- android_app/app/src/main/java/ch/gamesxmatch/main/ChatAdaptator.kt
+++ android_app/app/src/main/java/ch/gamesxmatch/main/ChatAdaptator.kt
@@ -1,11 +1,14 @@
 package ch.gamesxmatch.main
 
+import android.app.ActionBar
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.Space
-import android.widget.TextView
+import android.widget.*
+import androidx.core.view.MarginLayoutParamsCompat
+import androidx.core.view.marginBottom
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import ch.gamesxmatch.R
 
@@ -13,11 +16,10 @@ import ch.gamesxmatch.R
 class ChatAdaptator(val messages : ArrayList<String>) : RecyclerView.Adapter<ChatAdaptator.ViewHolder>() {
 
     companion object{
-        const val message_weight = 5
-        const val space_weight = 1
-        const val space_disabled_weight = 0
-        const val background_sender = "#add8e6"
-        const val background_receiver = "#A4C639"
+        const val margin_default = 50
+        const val margin_side = margin_default + 100
+        const val background_match = "#add8e6"
+        const val background_user = "#A4C639"
     }
 
     override fun getItemCount(): Int {
@@ -35,14 +37,28 @@ class ChatAdaptator(val messages : ArrayList<String>) : RecyclerView.Adapter<Cha
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var message : TextView = itemView.findViewById(R.id.message_message)
-        var leftSpace : Space = itemView.findViewById(R.id.message_left_space)
-        var rightSpace : Space = itemView.findViewById(R.id.message_right_space)
     }
 
     private fun bindValues(data : String, holder : ViewHolder) {
+        if(matchMessage(data)){
+            setMargins(margin_side, margin_default, holder.message)
+        }
+        else{
+            setMargins(margin_default, margin_side, holder.message)
+            holder.message.setBackgroundColor(Color.parseColor(background_user))
+        }
         holder.message.setText(data)
     }
 
+    private fun setMargins(left : Int, right : Int, message : TextView) {
+        val param = message.layoutParams as ViewGroup.MarginLayoutParams
+        param.setMargins(left,0,right,0)
+        message.layoutParams = param
+    }
 
+    private fun matchMessage(data: String) : Boolean {
+        // TODO
+        return data[0] == '1'
+    }
 
 }
