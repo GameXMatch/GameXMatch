a9fecd2397f0cd4590bedee5b8cf53f41904c122
Mario Tomic <mario-tomic@hotmail.com>
2022-08-30 10:33:08 +0200

    buttons working

--- android_app/app/src/main/java/ch/gamesxmatch/main/Chat.kt
+++ android_app/app/src/main/java/ch/gamesxmatch/main/Chat.kt
@@ -1,6 +1,9 @@
 package ch.gamesxmatch.main
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -15,23 +18,63 @@ class Chat : AppCompatActivity() {
     // TODO : Implement the firebase chat like app
     lateinit var debug_text : TextView
     lateinit var recyclerView: RecyclerView
+    lateinit var returnButton: ImageButton
+    lateinit var sendButton: ImageButton
+    lateinit var messageEditText: EditText
+
     var messages = ArrayList<String>(Arrays.asList("1test", "test2", "1test3", "1test4", "test5"))
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initComponents()
+        getMatchData()
+        setupMessageDisplay()
+    }
+
+    private fun initComponents(){
         setContentView(R.layout.activity_chat)
         debug_text = findViewById(R.id.chat_debug_text)
+        messageEditText = findViewById(R.id.chat_editText_message)
+        recyclerView = findViewById(R.id.chat_recyclerView)
+        setRetunButton()
+        setSendButton()
 
-        // Data from the
+    }
+
+    private fun getMatchData(){
+        // Data from the match activity
         val extras = intent.extras
         if (extras != null) {
             val value = extras.getString("matchID")
             debug_text.setText(value)
         }
 
-        recyclerView = findViewById(R.id.chat_recyclerView)
+        // TODO : Get messages and all the needed data
+    }
+
+    private fun setupMessageDisplay(){
         val chatAdaptator = ChatAdaptator(messages)
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = chatAdaptator
+    }
+
+    private fun setRetunButton(){
+        returnButton = findViewById(R.id.chat_imageButton_back)
+        returnButton.setOnClickListener{
+            finish()
+        }
+    }
+
+    private fun setSendButton(){
+        sendButton = findViewById(R.id.chat_send_imagebutton)
+        sendButton.setOnClickListener{
+            val message = messageEditText.text.toString()
+            sendMessage(message)
+        }
+    }
+
+    private fun sendMessage(message : String){
+        //TODO
+        println(message)
     }
 }
