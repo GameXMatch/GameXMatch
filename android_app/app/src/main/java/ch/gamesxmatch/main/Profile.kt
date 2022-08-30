dab911d43f6d1e9c2bf7181a2ba1b11ca58da562
Mario Tomic <mario-tomic@hotmail.com>
2022-08-30 14:33:40 +0200

    buttons enable/disable editText

--- android_app/app/src/main/java/ch/gamesxmatch/main/Profile.kt
+++ android_app/app/src/main/java/ch/gamesxmatch/main/Profile.kt
@@ -4,10 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageButton
 import androidx.fragment.app.Fragment
 import ch.gamesxmatch.R
+import java.util.zip.Inflater
 
 class Profile: Fragment() {
+
+    lateinit var nameButton: ImageButton
+    lateinit var descriptionButton: ImageButton
+    lateinit var userNameEditText: EditText
+    lateinit var descriptionEditText: EditText
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,6 +24,30 @@ class Profile: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_profile, container, false)
+        initViews(view)
         return view
+    }
+
+    private fun initViews(view : View) {
+        initUsernameViews(view)
+        initDescriptionViews(view)
+    }
+
+    private fun initUsernameViews(view : View){
+        userNameEditText = view.findViewById(R.id.profile_username_editText)
+        userNameEditText.isEnabled = false
+        nameButton = view.findViewById(R.id.profile_nameEdit_imageButton)
+        nameButton.setOnClickListener{
+            userNameEditText.isEnabled = !userNameEditText.isEnabled
+        }
+    }
+
+    private fun initDescriptionViews(view: View){
+        descriptionEditText = view.findViewById(R.id.profile_description_editText)
+        descriptionEditText.isEnabled = false
+        descriptionButton = view.findViewById(R.id.profile_description_imageButton)
+        descriptionButton.setOnClickListener{
+            descriptionEditText.isEnabled = !descriptionEditText.isEnabled
+        }
     }
 }
