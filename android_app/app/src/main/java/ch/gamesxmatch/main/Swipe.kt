0a3ebb91ce2f7529a9b2cddd1079ecc54b9cd537
JanisChiffelle <janischiffelle@hotmail.ch>
2022-09-01 09:46:36 +0200

    swipe work

--- android_app/app/src/main/java/ch/gamesxmatch/main/Swipe.kt
+++ android_app/app/src/main/java/ch/gamesxmatch/main/Swipe.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.LinearInterpolator
 import android.widget.Button
+import android.widget.StackView
 import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import ch.gamesxmatch.R
+import com.yuyakaido.android.cardstackview.*
 
-class Swipe : Fragment() {
+class Swipe : Fragment(), CardStackListener {
 
     // TODO : Create data type for the match
     // TODO : Find a way to display the data
@@ -18,7 +24,21 @@ class Swipe : Fragment() {
 
     lateinit var btnSwipeLeft : Button
     lateinit var btnSwipeRight : Button
-    lateinit var cardView: CardView
+    lateinit var stack_view: CardStackView
+
+    var profiles = ArrayList<Profile>(arrayListOf(Profile(123,"test1","desc1", arrayListOf("asdasd", "vcbcvbcvb")),
+        Profile(234,"test2","desc2", arrayListOf("asdgfdgd", "dfg")),
+        Profile(456,"test3","desc3", arrayListOf("sdfghg", "vcvxvcbcvdgfdfabcvb"))))
+
+    var adapter = SwipeAdapter(profiles)
+    lateinit var layoutManager: CardStackLayoutManager
+
+    data class Profile(
+        val uuid: Int,
+        val name: String,
+        val description: String,
+        val games_images: ArrayList<String>
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,7 +48,45 @@ class Swipe : Fragment() {
         val view = inflater.inflate(R.layout.fragment_swipe, container, false)
         btnSwipeLeft = view.findViewById(R.id.swipe_left_button)
         btnSwipeRight = view.findViewById(R.id.swipe_right_button)
-        cardView = view.findViewById(R.id.swipe_cardview)
+        stack_view = view.findViewById(R.id.stack_view_id)
+
+        layoutManager = CardStackLayoutManager(context).apply {
+            setSwipeableMethod(SwipeableMethod.AutomaticAndManual)
+            setOverlayInterpolator(LinearInterpolator())
+        }
+
+        stack_view.layoutManager = layoutManager
+        stack_view.adapter = adapter
+        stack_view.itemAnimator.apply {
+            if (this is DefaultItemAnimator) {
+                supportsChangeAnimations = false
+            }
+        }
+
         return view
+    }
+
+    override fun onCardDragging(direction: Direction?, ratio: Float) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCardSwiped(direction: Direction?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCardRewound() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCardCanceled() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCardAppeared(view: View?, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCardDisappeared(view: View?, position: Int) {
+        TODO("Not yet implemented")
     }
 }
