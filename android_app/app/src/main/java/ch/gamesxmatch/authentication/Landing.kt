eae87b8872c6887c228bf7c5101bc79465c3d843
Mario Tomic <mario-tomic@hotmail.com>
2022-09-06 17:47:43 +0200

    Landing refactor and comment

--- android_app/app/src/main/java/ch/gamesxmatch/authentication/Landing.kt
+++ android_app/app/src/main/java/ch/gamesxmatch/authentication/Landing.kt
@@ -19,49 +19,75 @@ import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
 
+/**
+ * Launch/initial activity. It gives the option to the user to log in. In the case of a successful
+ * login, the app will query the database in order to load the necessary data
+ *
+ * This activity transitions to :
+ *  - The login page, if the user didn't log in yet (Login)
+ *  - The main part of the app if the user is logged in (CoreApp)
+ */
 class Landing : AppCompatActivity() {
 
+    // Component
+    private lateinit var loginButton : Button
+
+    // Data
     private lateinit var db: FirebaseFirestore
     var sharedData = SharedData.getInstance()
-    // TODO : Check for credentials
-    // TODO : Nice text
-    private lateinit var loginButton : Button
     var uuid = sharedData.getMainUserUUID()
+
+    // TODO : Nice text
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         if(checkLoggedInt()){
-            val auth = FirebaseAuth.getInstance()
-            sharedData.setTempUUID(auth.currentUser?.uid.toString())
-            uuid = sharedData.getMainUserUUID()
-            getGames()
-            getUserData()
-            val intent = Intent(this, CoreApp::class.java)
-
-            Handler(Looper.getMainLooper()).postDelayed({
-                startActivity(intent)
-                finish()
-            }, 3000)
-
+            loadDataAndStart()
         } else {
-            setContentView(R.layout.activity_landing)
-            loginButton = findViewById(R.id.landing_connect_button)
-            val intent = Intent(this, Login::class.java)
-            loginButton.setOnClickListener {
-                startActivity(intent)
-                finish()
-            }
+            displayLogin()
         }
     }
 
-    fun getGames() {
+    /**
+     * Queries the database to get all the necessary data and starts the main app
+     */
+    private fun loadDataAndStart() {
+        val auth = FirebaseAuth.getInstance()
+        sharedData.setTempUUID(auth.currentUser?.uid.toString())
+        uuid = sharedData.getMainUserUUID()
+        getGames()
+        getUserData()
+        val intent = Intent(this, CoreApp::class.java)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+
+    /**
+     * Displays the view that allows the user to log in
+     */
+    private fun displayLogin() {
+        setContentView(R.layout.activity_landing)
+        loginButton = findViewById(R.id.landing_connect_button)
+        val intent = Intent(this, Login::class.java)
+        loginButton.setOnClickListener {
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    /**
+     * Queries the database to get the games and their images
+     */
+    private fun getGames() {
         db = Firebase.firestore
         val supportedGames = ArrayList<Game>()
         db.collection("Games")
             .get()
             .addOnSuccessListener { result ->
                 for (document in result) {
-                    println("${document.id} => ${document.data}")
                     val tmp = document.toObject<Game>()
                     tmp.id = document.id
                     supportedGames.add(tmp)
@@ -73,7 +99,10 @@ class Landing : AppCompatActivity() {
             }
     }
 
-    fun getUserData() {
+    /**
+     * Gets all the user's data
+     */
+    private fun getUserData() {
         val docRef = db.collection("Users").document(uuid)
         docRef.get()
             .addOnSuccessListener { document ->
@@ -100,8 +129,10 @@ class Landing : AppCompatActivity() {
             }
     }
 
-    fun checkLoggedInt() : Boolean {
-        // TODO
+    /**
+     * Checks if the user is logged in
+     */
+    private fun checkLoggedInt() : Boolean {
         return GoogleSignIn.getLastSignedInAccount(this) != null
     }
 
