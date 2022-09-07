9a8326b07c1712fdbb6178890db40a2e28ebe4a9
Mario Tomic <mario-tomic@hotmail.com>
2022-09-07 17:41:51 +0200

    comments done

--- android_app/app/src/main/java/ch/gamesxmatch/data/Game.kt
+++ android_app/app/src/main/java/ch/gamesxmatch/data/Game.kt
@@ -4,6 +4,10 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.util.Base64
 
+/**
+ * Stores the data necessary for the games. As an important note, the bitmap image comes
+ * in a base64 string and needs to be converted before usage.
+ */
 data class Game(
     var id : String = "",
     var name : String = "",
